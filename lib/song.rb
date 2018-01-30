626bcab8342ab5a711c050d409cce2d3228515b1
Jennifer Dobak <jdobak1@gmail.com>
2018-01-30 19:07:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,14 +11,17 @@ end
 def self.new_by_filename(file)
   #parse the song and artist which are separated by -
   parts = file.split(/\ - |\./)
-  artist_name = parts[0]
+  artist = parts[0]
   song_name = parts[1]
 
   song = self.new(song_name)
   song.name = song_name
-  song.artist = Artist.find_or_create_by_name(artist_name)
+  song.artist_name = Artist.find_or_create_by_name(artist_name)
   artist.add_song(self)
   song
 end
 
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
 end
