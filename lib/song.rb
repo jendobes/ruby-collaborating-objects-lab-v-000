e4574ace0eb104429d3fd250f965f8310e9a645a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-30 18:48:26 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Song
+
+attr_accessor :artist, :name
+
+def initialize(name)
+  @name = name
+end
+
+def self.new_by_filename(file)
+  #parse the song and artist which are separated by -
+  parts = file.split(/\ - |\./)
+  artist_name = parts[0]
+  song_name = parts[1]
+
+  song = self.new(song_name)
+  song.name = song_name
+  song.artist.name = artist_name
+  song
+end
+
+end
