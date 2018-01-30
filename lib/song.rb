683e1d7e9e5e2f92a7ec44ae8eda652453d56ed5
Jennifer Dobak <jdobak1@gmail.com>
2018-01-30 19:08:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,7 @@ def self.new_by_filename(file)
 
   song = self.new(song_name)
   song.name = song_name
-  song.artist_name = Artist.find_or_create_by_name(artist_name)
-  artist.add_song(self)
+  song.artist = artist_name(artist)
   song
 end
 
